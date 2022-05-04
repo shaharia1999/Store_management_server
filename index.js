fe6035951931e5958f5d3a3dde6d9c014732ae24
Shaharia Ahmed <adeptshaharia@gmail.com>
2022-05-04 21:15:04 +0600

    back

--- index.js
+++ index.js
@@ -99,6 +99,18 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
             const result = await collection.updateOne(filter, updateDoc, options);
             res.send(result);
       })
+        app.put('/product/increase/:id',async(req,res)=>{
+          const id=req.params.id;
+          const data=req.body;
+          console.log(data);
+          const filter =  {_id: id} ;
+          console.log("filter",filter);
+          const options = { upsert: false };
+          const updateDoc = {
+             $inc: { quantity:data.amount} }
+            const result = await collection.updateOne(filter, updateDoc, options);
+            res.send(result);
+      })
      
     
       // delete api
